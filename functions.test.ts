a587c9c164f62608cc5764b440cada49a3a2b560
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-18 11:29:50 -0600

    Part 3: Unit Test complete, trello-link.txt now has link in it.

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,15 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test("Function shuffleArray should return an array that is the same length as the array that was input", () =>{
+        let testArr = [1,2,3,4,5,6,7]
+        let randArr = shuffleArray(testArr)
+        expect(randArr.length).toEqual(randArr.length)
+    })
+
+    test("Function shuffleArray should actually randomize the array values", () => {
+        let testArr = [7,5,4,3,2,1]
+        let randArr = shuffleArray(testArr)
+        expect(randArr).not.toBe(testArr)
+    })
 })

84e2a78e0969aa72e2da88071ee5a9365c5e3aea
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-18 12:32:13 -0600

    Part 4:Write Automated Tests complete.

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,19 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test("Test to see if the 'draw' button shows the 'choices' div", async () => {
+    await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+    await driver.sleep(2000)
+    const botChoices = await driver.findElement(By.xpath('//div[@id="choices"]'))
+    expect(botChoices.isDisplayed()).toBeTruthy()
+})
+
+test("Test to see if the 'Add to Duo' Button displays the 'player duo' div", async () => {
+    await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+    await driver.sleep(2000)
+    await driver.findElement(By.xpath('//body/section[1]/div/div[1]/button')).click()
+    await driver.sleep(2000)
+    const playerDuo = await driver.findElement(By.xpath('//div[@id="player-duo"]'))
+    expect(playerDuo.isDisplayed()).toBeTruthy()
+})
